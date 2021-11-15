64ec89bd425bfa95aa355f6defa5a8d74c07c48e
DanielArturoAlejoAlvarez <yourmediasoft@gmail.com>
2021-11-15 18:29:04 -0500

    Creating a HelloAPIView class like intro view Application

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+class HelloAPIView(APIView):
+    def get(self, request, format=None):
+
+        an_apiview = [
+            'We use HTTP methods like get,post,put,patch,delete,etc',
+            'It is similar a to traditional view in django',
+            'It gives us greater control over the logic of the application',
+            'It is manually mapped to Urls'
+        ]
+
+        return Response({
+            'msg': 'Hello World!', 
+            'an_apiview': an_apiview
+        })
+
