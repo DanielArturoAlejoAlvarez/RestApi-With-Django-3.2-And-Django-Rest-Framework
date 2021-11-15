a13ba824c8c72ee77160b158c370394a9c9dcf93
DanielArturoAlejoAlvarez <yourmediasoft@gmail.com>
2021-11-15 10:46:49 -0500

    Creating an Application and registered on settings project

---
+++ profiles_api/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ProfilesApiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'profiles_api'
--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+    'rest_framework.authtoken',
+
+    'profiles_api',
 ]
 
 MIDDLEWARE = [
