b54a673a2bbde43c22c670aff51d3850cd0a4400
DanielArturoAlejoAlvarez <yourmediasoft@gmail.com>
2021-11-15 11:27:23 -0500

    Creating a custom model User

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+
+class UserProfileManager():
+    pass
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255),
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserProfileManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    def get_full_name(self):
+        return self.name
+
+    def get_short_name(self):
+        return self.name 
+
+    def __str__(self):
+        return self.email
+
+
