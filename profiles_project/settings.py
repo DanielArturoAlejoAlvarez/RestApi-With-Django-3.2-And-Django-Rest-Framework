f32d97b8b6ef6ccb2ad8936d07ee33297393dd85
DanielArturoAlejoAlvarez <yourmediasoft@gmail.com>
2021-11-15 12:11:09 -0500

    Registry const AUTH_USER_MODEL to Authentication of Users in settings project

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -128,3 +128,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Authentication of Users
+AUTH_USER_MODEL = 'profiles_api.UserProfile'
