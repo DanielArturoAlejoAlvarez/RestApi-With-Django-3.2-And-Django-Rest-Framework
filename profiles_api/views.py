50f5248753bc58233f3182e8d64a2e2b4363975d
DanielArturoAlejoAlvarez <yourmediasoft@gmail.com>
2021-11-15 19:19:58 -0500

    Creating a post function in HelloAPIView

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -3,7 +3,13 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+from rest_framework import status
+from profiles_api import serializers
+
 class HelloAPIView(APIView):
+    serializer_class = serializers.HelloSerializer
+
+
     def get(self, request, format=None):
 
         an_apiview = [
@@ -17,4 +23,22 @@ class HelloAPIView(APIView):
             'msg': 'Hello World!', 
             'an_apiview': an_apiview
         })
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            msg = f'Hello {name}'
+
+            return Response({
+                'msg': msg
+            })
+
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
 
